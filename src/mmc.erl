83922ef10f58f31e8fac2ad410dc7643e2e86af5
Dmitry Karpov <d.karpov@i-free.com>
2013-04-12 20:00:26 +0400

    typo fix

--- src/mmc.erl
+++ src/mmc.erl
@@ -1,50 +1,48 @@
 -module(mmc).
 -author('komm@siphost.su').
 
--export([start/0, start/1, pstree/1, visual/3]).
+-export([s/0, s/1, start/0, start/1, pstree/1]).
 
 name(Pid, {registered_name,List})->
   name(Pid, List);
 name(Pid, []) -> 
   pid_to_list(Pid);
-name(Pid, Name) when is_atom(Name) ->
+name(_Pid, Name) when is_atom(Name) ->
   atom_to_list(Name)
 .
 
 links(Pid, {links,List})->
   links(Pid, List);
-links(Pid, [])->
+links(_, [])->
   [];
 links(Pid, [Link|Links])->
   case ets:lookup(mmc, Pid) of
-  [{Pid, Link}] -> [];
+  [{_, Link}] -> [];
   _ -> [pstree(Link)|links(Pid, Links)]
   end
 .
 
-visual(GlobalPrefix, LocalPrefix, [])-> ok;
+visual(_GlobalPrefix, _LocalPrefix, [])-> ok;
 visual(GlobalPrefix, LocalPrefix, [ {Pid, LinkedPid} | Tail])->
-	case LinkedPid of
-	[[]] -> 
-		io:format('~s~s+---~s~n',[GlobalPrefix, LocalPrefix, Pid]),
-		visual(GlobalPrefix, LocalPrefix, Tail);
-	PidList when is_list(PidList)->
-		io:format('~s~s+---~s---+~n',[GlobalPrefix, LocalPrefix, Pid]),
-		NewGlobalPrefix = 
-		case Tail of
-		[] -> GlobalPrefix ++ lists:map(fun(C)-> $\  end, LocalPrefix);
-		[[]] -> GlobalPrefix ++ lists:map(fun(C)-> $\  end, LocalPrefix);
-		_ -> 
-			GlobalPrefix ++ lists:map(fun(C)->$\  end, LocalPrefix) ++ [$|]
-		end,
-		visual(NewGlobalPrefix, lists:duplicate(length(Pid)+7," "), LinkedPid),
-		%%visual(NewGlobalPrefix, lists:duplicate(length(Pid)+6," ") ++ ["+---"], LinkedPid),
-		visual(GlobalPrefix, LocalPrefix, Tail);
-	_ -> visual(GlobalPrefix, LocalPrefix, Tail)
-	end
+  case LinkedPid of
+  [[]] -> 
+    io:format('~s~s+---~s~n',[GlobalPrefix, LocalPrefix, Pid]);
+  PidList when is_list(PidList)->
+    io:format('~s~s+---~s---+~n',[GlobalPrefix, LocalPrefix, Pid]),
+    NewGlobalPrefix = 
+    case Tail of
+    [] -> GlobalPrefix ++ lists:map(fun(_)-> $\  end, LocalPrefix);
+    [[]] -> GlobalPrefix ++ lists:map(fun(_)-> $\  end, LocalPrefix);
+    _ -> 
+      GlobalPrefix ++ lists:map(fun(_)->$\  end, LocalPrefix) ++ [$|]
+    end,
+    visual(NewGlobalPrefix, lists:duplicate(length(Pid)+7," "), LinkedPid);
+  _ -> ok
+  end,
+  visual(GlobalPrefix, LocalPrefix, Tail)
 ;
 visual(GlobalPrefix, LocalPrefix, [ _ | Tail])->
-	visual(GlobalPrefix, LocalPrefix, Tail)
+  visual(GlobalPrefix, LocalPrefix, Tail)
 .
 
 -spec pstree(Pid :: list() | pid())-> process_undefined | term().
@@ -57,7 +55,7 @@ pstree(Pid) when is_pid(Pid)->
     ets:insert(mmc, {Pid, GroupLeader}),
     case erlang:process_info(Pid) of
       undefined -> process_undefined;
-      Parameters ->
+      _Parameters ->
         {name(Pid, erlang:process_info(Pid, registered_name)),
   	  links(Pid, erlang:process_info(Pid, links))
         }
@@ -66,17 +64,21 @@ pstree(Pid) when is_pid(Pid)->
   end
 .
 
--spec start()-> term().
--spec start(Pid :: list() | pid())-> term().
+-spec s()-> ok.
+-spec s(Pid :: list() | pid())-> ok.
+s()-> start().
+s(Pid)-> start(Pid).
+
+-spec start()-> ok.
+-spec start(Pid :: list() | pid())-> ok.
 start()->
   start(self()).
 start(Pid) when is_list(Pid)->
-  start(list_to_atom(Pid));
+  start(list_to_pid(Pid));
 start(Pid) when is_pid(Pid)->
   ets:new(mmc,[ set , public,named_table]),
   Tree = pstree(Pid),
-%%  io:format('---',[]),
   visual("", "", [Tree]),
-  ets:delete(mmc)
-
+  ets:delete(mmc),
+  ok
 .
